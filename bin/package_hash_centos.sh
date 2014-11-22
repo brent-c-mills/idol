8c89cad7984d0e22e4546ab3ee5762532ef77e25
kaizoku0506 <brent.c.mills@gmail.com>
2014-11-22 13:37:02 -0500

    Add functions to package_hash scripts.

--- bin/package_hash_centos.sh
+++ bin/package_hash_centos.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
 
+set -e
+
+handoff() {
+    echo "package_hash_centos.sh has been kicked off by idol_create.sh..." | tee -a $LOG_OUT;
+    echo "package_hash_centos.sh is initiating package hash BATS creation..." | tee -a $LOG_OUT;
+    echo "idol name.................."$IDOL_NAME | tee -a $LOG_OUT;
+    echo "" | tee -a $LOG_OUT;
+}
+
+completion() {
+    echo "package_hash_centos.sh has completed for idol "$IDOL_NAME | tee -a $LOG_OUT;
+}
+
+initialize_bats() {
+    echo "#!/usr/bin/env bats" >> $OUTPUT_BATS
+    echo "" >> $OUTPUT_BATS
+    echo "load test_helper" >> $OUTPUT_BATS
+    echo "fixtures bats" >> $OUTPUT_BATS
+    echo "" >> $OUTPUT_BATS
+}
+
+generate_package_hash() {
+    rm -f /tmp/package.txt && touch /tmp/package.txt
+	rpm -qa >> /tmp/packge.txt
+	local hashgolden=($(md5sum /tmp/package.txt))
+	echo $hashgolden
+}
+
+generate_package_hash_bats() {
+    echo "@test \"SOFTWARE CHECK - "${IDOL_NAME}" Package HASH\" {" >> $OUTPUT_BATS
+    echo "rpm -qa > /tmp/packge.txt" >> $OUTPUT_BATS
+    echo "HASHCHECK=($(md5sum /tmp/package.txt))" >> $OUTPUT_BATS
+    echo "[ $HASHCHECK -eq ${HASHGOLDEN} ]" >> $OUTPUT_BATS
+    echo "}" >> $OUTPUT_BATS
+    echo " " >> $OUTPUT_BATS
+}
+
+
 BASE_DIR=$1;
 IDOL_NAME=$2;
 LOG_OUT=$3;
@@ -11,28 +49,13 @@ IDOL_DIR=$TEST_DIR/$IDOL_NAME;
 
 OUTPUT_BATS=$IDOL_DIR/package_hash.bats
 
-#Initialize .bats file
+#Acknowledge handoff...
+handoff
 
-echo "#!/usr/bin/env bats" >> $OUTPUT_BATS
-echo "" >> $OUTPUT_BATS
-echo "load test_helper" >> $OUTPUT_BATS
-echo "fixtures bats" >> $OUTPUT_BATS
-echo "" >> $OUTPUT_BATS
+#Initialize bats and generate package list / package bats.
+initialize_bats
+HASHGOLDEN=$(generate_package_hash)
+generate_package_hash_bats
 
-#Generating Initial Package List
-rm -f /tmp/package.txt && touch /tmp/package.txt
-rm -f /tmp/failedlist.txt && touch /tmp/failedlist.txt
-rpm -qa >> /tmp/packge.txt
-
-HASHGOLDEN=($(md5sum /tmp/package.txt))
-
-#GENERATE TEST
-
-    echo "@test \"SOFTWARE CHECK - "${IDOL_NAME}" Package HASH\" {" >> $OUTPUT_BATS
-    echo "rpm -qa > /tmp/packge.txt" >> $OUTPUT_BATS
-    echo "HASHCHECK=($(md5sum /tmp/package.txt))" >> $OUTPUT_BATS
-    echo "[ $HASHCHECK -eq ${HASHGOLDEN} ]" >> $OUTPUT_BATS
-    echo "}" >> $OUTPUT_BATS
-    echo " " >> $OUTPUT_BATS
-
-echo "Finished Generating Package Hash Test for "${IDOL_NAME} | tee -a $LOG_OUT;
+#Acknowledge completion of BATS generation.
+completion
--- bin/package_hash_darwin.sh
+++ bin/package_hash_darwin.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
 
+set -e
+
+handoff() {
+    echo "package_hash_darwin.sh has been kicked off by idol_create.sh..." | tee -a $LOG_OUT;
+    echo "package_hash_darwin.sh is initiating package hash BATS creation..." | tee -a $LOG_OUT;
+    echo "idol name.................."$IDOL_NAME | tee -a $LOG_OUT;
+    echo "" | tee -a $LOG_OUT;
+}
+
+completion() {
+    echo "package_hash_darwin.sh has completed for idol "$IDOL_NAME | tee -a $LOG_OUT;
+}
+
+initialize_bats() {
+    echo "#!/usr/bin/env bats" >> $OUTPUT_BATS
+    echo "" >> $OUTPUT_BATS
+    echo "load test_helper" >> $OUTPUT_BATS
+    echo "fixtures bats" >> $OUTPUT_BATS
+    echo "" >> $OUTPUT_BATS
+}
+
+generate_package_hash() {
+    rm -f /tmp/package.txt && touch /tmp/package.txt
+	ls /Applications/ >> /tmp/packge.txt
+	local hashgolden=($(md5sum /tmp/package.txt))
+	echo $hashgolden
+}
+
+generate_package_hash_bats() {
+    echo "@test \"SOFTWARE CHECK - "${IDOL_NAME}" Package HASH\" {" >> $OUTPUT_BATS
+    echo "ls /Applications/ > /tmp/packge.txt" >> $OUTPUT_BATS
+    echo "HASHCHECK=($(md5sum /tmp/package.txt))" >> $OUTPUT_BATS
+    echo "[ $HASHCHECK -eq ${HASHGOLDEN} ]" >> $OUTPUT_BATS
+    echo "}" >> $OUTPUT_BATS
+    echo " " >> $OUTPUT_BATS
+}
+
+
 BASE_DIR=$1;
 IDOL_NAME=$2;
 LOG_OUT=$3;
@@ -10,34 +48,14 @@ MAN_DIR=$BASE_DIR/man;
 IDOL_DIR=$TEST_DIR/$IDOL_NAME;
 
 OUTPUT_BATS=$IDOL_DIR/package_hash.bats
-FAILEDLIST=/tmp/failedlist.txt
 
-PROCESSED=0
-SUCCESS=0
-FAIL=0
+#Acknowledge handoff...
+handoff
 
-#Initialize .bats file
+#Initialize bats and generate package list / package bats.
+initialize_bats
+HASHGOLDEN=$(generate_package_hash)
+generate_package_hash_bats
 
-echo "#!/usr/bin/env bats" >> $OUTPUT_BATS
-echo "" >> $OUTPUT_BATS
-echo "load test_helper" >> $OUTPUT_BATS
-echo "fixtures bats" >> $OUTPUT_BATS
-echo "" >> $OUTPUT_BATS
-
-#Generating Initial Package List
-rm -f /tmp/package.txt && touch /tmp/package.txt
-rm -f /tmp/failedlist.txt && touch /tmp/failedlist.txt
-ls /Applications/ >> /tmp/packge.txt
-
-HASHGOLDEN=($(md5sum /tmp/package.txt))
-
-#GENERATE TEST
-
-    echo "@test \"SOFTWARE CHECK - "${IDOL_NAME}" Package HASH\" {" >> $OUTPUT_BATS
-    echo "ls /Applications/ > /tmp/packge.txt" >> $OUTPUT_BATS
-    echo "HASHCHECK=($(md5sum /tmp/package.txt))" >> $OUTPUT_BATS
-    echo "[ $HASHCHECK -eq ${HASHGOLDEN} ]" >> $OUTPUT_BATS
-    echo "}" >> $OUTPUT_BATS
-    echo " " >> $OUTPUT_BATS
-
-echo "Finished Generating Package Hash Test for "${IDOL_NAME} | tee -a $LOG_OUT;
+#Acknowledge completion of BATS generation.
+completion
