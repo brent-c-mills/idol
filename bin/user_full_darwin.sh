67627d04a337984a95c668cc3ea6a9519d7e434e
kaizoku0506 <brent.c.mills@gmail.com>
2014-11-22 16:58:37 -0500

    Add first version of user BATS generation scripts.

---
+++ bin/user_full_darwin.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -e
+
+handoff() {
+    echo "user_full_darwin.sh has been kicked off by idol_create.sh..." | tee -a $LOG_OUT;
+    echo "user_full_darwin.sh is initiating full user BATS creation..." | tee -a $LOG_OUT;
+    echo "idol name.................."$IDOL_NAME | tee -a $LOG_OUT;
+    echo "users to be processed..."$(ls /Applications | wc -l) | tee -a $LOG_OUT;
+    echo "" | tee -a $LOG_OUT;
+}
+
+completion() {
+    echo "user_full_darwin.sh has completed for idol "$IDOL_NAME | tee -a $LOG_OUT;
+}
+
+initialize_bats() {
+    echo "#!/usr/bin/env bats" >> $OUTPUT_BATS;
+    echo "" >> $OUTPUT_BATS;
+    echo "load test_helper" >> $OUTPUT_BATS;
+    echo "fixtures bats" >> $OUTPUT_BATS;
+    echo "" >> $OUTPUT_BATS;
+}
+
+generate_user_list() {
+    rm -f /tmp/user.txt && touch /tmp/user.txt;
+    cat /etc/passwd >> /tmp/user.txt;
+}
+
+generate_user_bats() {
+    while IFS=, read -r user; do
+
+        username=$(echo $user | cut -d: -f1);
+        user=$user;
+
+        echo "Adding user_full test for "${user} >> $LOG_OUT;
+        echo "@test \"SOFTWARE CHECK - "${user}"\" {" >> $OUTPUT_BATS;
+        echo "cat /etc/passwd | grep \""${user}"\"" >> $OUTPUT_BATS;
+        echo "[ \$? -eq 0 ]" >> $OUTPUT_BATS;
+        echo "}" >> $OUTPUT_BATS;
+        echo " " >> $OUTPUT_BATS;
+
+    done < /tmp/user.txt;
+}
+
+
+FULL_BATS=$1;
+IDOL_NAME=$2;
+LOG_OUT=$3;
+
+OUTPUT_BATS=$FULL_BATS/user_full.bats;
+
+#Acknowledge handoff...
+handoff;
+
+#Initialize bats and generate user list / user bats.
+initialize_bats;
+generate_user_list;
+generate_user_bats;
+
+#Acknowledge completion of BATS generation.
+completion;
---
+++ bin/user_hash_ubuntu.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+handoff() {
+    echo "user_hash_ubuntu.sh has been kicked off by idol_create.sh..." | tee -a $LOG_OUT;
+    echo "user_hash_ubuntu.sh is initiating user hash BATS creation..." | tee -a $LOG_OUT;
+    echo "idol name.................."$IDOL_NAME | tee -a $LOG_OUT;
+    echo "" | tee -a $LOG_OUT;
+}
+
+completion() {
+    echo "user_hash_ubuntu.sh has completed for idol "$IDOL_NAME | tee -a $LOG_OUT;
+    exit 0;
+}
+
+initialize_bats() {
+    echo "#!/usr/bin/env bats" >> $OUTPUT_BATS;
+    echo "" >> $OUTPUT_BATS;
+    echo "load test_helper" >> $OUTPUT_BATS;
+    echo "fixtures bats" >> $OUTPUT_BATS;
+    echo "" >> $OUTPUT_BATS;
+}
+
+generate_user_hash() {
+    echo "Generating user Golden Hash for "${IDOL_NAME} >> $LOG_OUT;
+    rm -f /tmp/user.txt && touch /tmp/user.txt;
+	cat /etc/passwd >> /tmp/user.txt;
+	local hashgolden=($(md5sum /tmp/user.txt));
+	echo $hashgolden;
+}
+
+generate_user_hash_bats() {
+    echo "Generating user Hash Test for "${IDOL_NAME} >> $LOG_OUT;
+    echo "@test \"SOFTWARE CHECK - "${IDOL_NAME}" user HASH\" {" >> $OUTPUT_BATS;
+    echo "cat /etc/passwd > /tmp/user.txt" >> $OUTPUT_BATS;
+    echo "HASHCHECK=($(md5sum /tmp/user.txt))" >> $OUTPUT_BATS;
+    echo "[ $HASHCHECK -eq ${HASHGOLDEN} ]" >> $OUTPUT_BATS;
+    echo "}" >> $OUTPUT_BATS;
+    echo " " >> $OUTPUT_BATS;
+}
+
+
+HASH_BATS=$1;
+IDOL_NAME=$2;
+LOG_OUT=$3;
+
+OUTPUT_BATS=$HASH_BATS/user_full.bats;
+
+#Acknowledge handoff...
+handoff;
+
+#Initialize bats and generate user list / user bats.
+initialize_bats;
+HASHGOLDEN=$(generate_user_hash);
+generate_user_hash_bats;
+
+#Acknowledge completion of BATS generation.
+completion;
