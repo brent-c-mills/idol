42a0795ba48d4525a322fd42d515423f6592f45e
kaizoku0506 <brent.c.mills@gmail.com>
2014-11-25 14:25:36 -0500

    Modify idol_test to resolve logging issue where only final test was logged.  Remove second from file names for logs.

--- bin/idol_test.sh
+++ bin/idol_test.sh
@@ -3,21 +3,21 @@
 set -e;
 
 handoff() {
-    echo "idol_test.sh has been kicked off by idol_create.sh..." | tee -a $LOG_OUT;
-    echo "idol_test.sh is initiating hash and full bats tests..." | tee -a $LOG_OUT;
-    echo "idol name:			"$IDOL_NAME | tee -a $LOG_OUT;
-    echo "" | tee -a $LOG_OUT;
+    echo "idol_test.sh has been kicked off by idol_create.sh..." | tee -a ${LOG_OUT};
+    echo "idol_test.sh is initiating hash and full bats tests..." | tee -a ${LOG_OUT};
+    echo "idol name:	"$IDOL_NAME | tee -a ${LOG_OUT};
+    echo "" | tee -a ${LOG_OUT};
 }
 
 completion() {
-    echo "idol_test.sh has completed testing for idol "$IDOL_NAME | tee -a $LOG_OUT;
+    echo "idol_test.sh has completed testing for idol "$IDOL_NAME | tee -a ${LOG_OUT};
     exit 0;
 }
 
 verify_idol() {
-	echo "Verifying Idol "${IDOL_NAME}"..." | tee -a $LOG_OUT;
+	echo "Verifying Idol "${IDOL_NAME}"..." | tee -a ${LOG_OUT};
 	if [[ ! -e ${TEST_DIR}/${IDOL_NAME} ]]; then
-		echo "Idol "${IDOL_NAME}" not found." | tee -a $LOG_OUT; 
+		echo "Idol "${IDOL_NAME}" not found." | tee -a ${LOG_OUT}; 
 		exit 1;
 	fi
 }
@@ -26,7 +26,7 @@ hash_bats_test() {
 
 	local hash_bats_name=$(basename ${hashbats});
 	IFS=_ read bats_category bats_type <<< $hash_bats_name;
-	echo "Hash test for "${hash_bats_name} > ${LOG_OUT};
+	echo "Hash test for "${hash_bats_name} | tee -a ${LOG_OUT};
 	echo "";
 	echo "Testing hash test for "${hash_bats_name}" on Idol "${IDOL_NAME} | tee -a ${LOG_OUT};
 	echo "===================" | tee -a ${LOG_OUT};
@@ -35,6 +35,9 @@ hash_bats_test() {
         echo ${hash_bats_name}" does not match the golden image for this Idol.  Full tests will be run." | tee -a ${LOG_OUT};
         echo "" | tee -a ${LOG_OUT};
         full_bats_test ${bats_category};
+    else
+    	echo ${hash_bats_name}" matches the golden image for this Idol." | tee -a ${LOG_OUT};
+        echo "" | tee -a ${LOG_OUT};
 	fi
 
 }
@@ -43,7 +46,7 @@ full_bats_test() {
 	bats_category=$1;
 	fullbats="${bats_category}_full.bats";
 
-	echo "A full bats test for "${IDOL_NAME}" : "${bats_category}" has been triggered." > ${LOG_OUT};
+	echo "A full bats test for "${IDOL_NAME}" : "${bats_category}" has been triggered." | tee -a ${LOG_OUT};
 	echo "";
 	echo "Commencing full test for "${fullbats}" on Idol "${IDOL_NAME} | tee -a ${LOG_OUT};
 	echo "===================" | tee -a ${LOG_OUT};
@@ -73,11 +76,15 @@ IDOL_NAME=$1;
 BASE_DIR=$2;
 LOG_OUT=$3;
 
-NOW=$(date +"%m_%d_%Y_%H%M%S");
 TEST_DIR=$BASE_DIR/tests;
 IDOL_DIR=$TEST_DIR/$IDOL_NAME;
 FULL_BATS=$IDOL_DIR/full_bats;
 HASH_BATS=$IDOL_DIR/hash_bats;
+
+#################################
+##       ACCEPT HANDOFF        ##
+#################################
+handoff;
 
 #################################
 ##    VERIFY IDOL EXISTANCE    ##
@@ -98,3 +105,8 @@ done
 #################################
 
 #Called from within hash tests.  This is due to the fact that full tests should only be run if hash tests fail (to save time).
+
+#################################
+##         COMPLETION          ##
+#################################
+completion;
