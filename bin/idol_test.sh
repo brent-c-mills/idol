ad29a95be8ece77e2d3b4c45549ff975034da191
kaizoku0506 <brent.c.mills@gmail.com>
2014-12-03 10:37:36 -0500

    Update logging for uniformity between hash_test messages and full_test messages.

--- bin/idol_test.sh
+++ bin/idol_test.sh
@@ -28,7 +28,7 @@ touch ${FAIL_LIST};
 
 while IFS='--' read -r LINE notused
 do
-        echo $LINE >> $FAIL_LIST;
+        echo ${LINE} >> ${FAIL_LIST};
 done < <(grep -A 2 "not ok" ${LOG_OUT});
 
 
@@ -38,12 +38,13 @@ full_bats_test() {
 	bats_category=$1;
 	fullbats="${bats_category}_full.bats";
 
+	echo "**" | tee -a ${LOG_OUT};
 	echo "** A full bats test for "${IDOL_NAME}" : "${bats_category}" has been triggered." | tee -a ${LOG_OUT};
 	echo "" | tee -a ${LOG_OUT};
 	echo "========================================" | tee -a ${LOG_OUT};
 	echo "Commencing full test for "${fullbats}" on Idol "${IDOL_NAME} | tee -a ${LOG_OUT};
 	echo "" | tee -a ${LOG_OUT};
-	bats $FULL_BATS/${fullbats} | tee -a ${LOG_OUT}
+	bats ${FULL_BATS}/${fullbats} | tee -a ${LOG_OUT}
 	echo "" | tee -a ${LOG_OUT};
 	echo "Completed full bats test for "${IDOL_NAME}" : "${bats_category}"." | tee -a ${LOG_OUT};
 }
@@ -51,23 +52,24 @@ full_bats_test() {
 handoff() {
     echo "idol_test.sh has been kicked off by idol_create.sh..." | tee -a ${LOG_OUT};
     echo "idol_test.sh is initiating hash and full bats tests..." | tee -a ${LOG_OUT};
-    echo "idol name:	"$IDOL_NAME | tee -a ${LOG_OUT};
+    echo "idol name:	"${IDOL_NAME} | tee -a ${LOG_OUT};
     echo "" | tee -a ${LOG_OUT};
 }
 
 hash_bats_test() {
 
 	local hash_bats_name=$(basename ${hashbats});
-	IFS=_ read bats_category bats_type <<< $hash_bats_name;
+	IFS=_ read bats_category bats_type <<< ${hash_bats_name};
 	echo "" | tee -a ${LOG_OUT};
 	echo "========================================" | tee -a ${LOG_OUT};
 	echo "Commencing hash test for "${hash_bats_name}" on Idol "${IDOL_NAME} | tee -a ${LOG_OUT};
-
+	echo "" | tee -a ${LOG_OUT};
 	if [[ $(bats ${hashbats} | tee -a ${LOG_OUT}) == *"not ok"* ]]; then
         echo ${hash_bats_name}" does not match the golden image for this Idol.  Full tests will be run." | tee -a ${LOG_OUT};
         full_bats_test ${bats_category};
     else
     	echo ${hash_bats_name}" matches the golden image for this Idol." | tee -a ${LOG_OUT};
+    	echo "Completed hash bats test for "${IDOL_NAME}" : "${bats_category}"." | tee -a ${LOG_OUT};
 	fi
 
 }
@@ -90,7 +92,7 @@ verify_idol() {
 
 EXPECTED_ARGS=3;
 
-if [ $# -ne $EXPECTED_ARGS ]; then
+if [ $# -ne ${EXPECTED_ARGS} ]; then
 	echo "Something has gone wrong.  The script idol_test.sh expected 3 arguments from idol, but only received "$#".";
 	exit 1;
 fi
@@ -104,10 +106,10 @@ IDOL_NAME=$1;
 BASE_DIR=$2;
 LOG_OUT=$3;
 
-TEST_DIR=$BASE_DIR/tests;
-IDOL_DIR=$TEST_DIR/$IDOL_NAME;
-FULL_BATS=$IDOL_DIR/full_bats;
-HASH_BATS=$IDOL_DIR/hash_bats;
+TEST_DIR=${BASE_DIR}/tests;
+IDOL_DIR=${TEST_DIR}/${IDOL_NAME};
+FULL_BATS=${IDOL_DIR}/full_bats;
+HASH_BATS=${IDOL_DIR}/hash_bats;
 
 #################################
 ##       ACCEPT HANDOFF        ##
