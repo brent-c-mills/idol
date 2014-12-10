87816ac33f7ca0953790340df6eded9185d62f94
kaizoku0506 <brent.c.mills@gmail.com>
2014-12-10 11:48:46 -0500

    Update idol_list.sh to output more information about each idol.  New output is in a table with idol name, idol os, idol creation date, and idol author.

--- bin/idol_list.sh
+++ bin/idol_list.sh
@@ -11,15 +11,23 @@ TEST_DIR=$1;
 echo "idol.sh";
 echo "";
 
-echo "${bold}	CURRENT IDOLS	${normal}";
+echo "${bold}			CURRENT IDOLS	${normal}";
 echo "";
 
 if [ ! "$(ls -A /$1)" ]; then
 	echo "  NO IDOLS  ";
 else
-	available=$(ls -1 ${TEST_DIR});
-	for i in available[@]; do
-	    echo "  "$available;
+	printf "${bold}%-15s | %-15s | %-15s | %-15s\n " "  IDOL" "OS" "DATE" "AUTHOR";
+	echo "${normal}------------------------------------------------------------";
+
+	available=(`ls -1 ${TEST_DIR}`);
+	for i in ${available[@]}; do
+	    IDOLNAME=$(grep "NAME" ${TEST_DIR}/${i}/README.txt | awk -F':' '{ print $2 }');
+		IDOLOS=$(grep "OS:" ${TEST_DIR}/${i}/README.txt | awk -F':' '{ print $2 }');
+		IDOLDATE=$(grep "DATE" ${TEST_DIR}/${i}/README.txt | awk -F':' '{ print $2 }');
+		IDOLAUTHOR=$(grep "AUTHOR" ${TEST_DIR}/${i}/README.txt | awk -F':' '{ print $2 }');
+
+		printf "%-15s | %-15s | %-15s | %-15s\n" "  "${IDOLNAME} ${IDOLOS} ${IDOLDATE} ${IDOLAUTHOR};
 	done
 fi
 echo "";
